e4a95b40d4b425ff586e0b3aaaf1ed581d215b79
将之大西 <omccello@gmail.com>
2019-06-03 20:54:15 +0900

    Viewの表示を仮修正

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,26 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-
-  describe "GET #home" do
+  describe "#home" do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #about" do
+  describe "#about" do
     it "returns http success" do
       get :about
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #tos" do
+  describe "#tos" do
     it "returns http success" do
       get :tos
       expect(response).to have_http_status(:success)
     end
   end
-
 end
