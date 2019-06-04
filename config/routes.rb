e10917dbed3639eda17fec226b2a08a6a2802cd1
将之大西 <omccello@gmail.com>
2019-06-04 12:25:11 +0900

    GCEの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/tos'
+  get '/about',   to: 'static_pages#about'
+  get '/tos',     to: 'static_pages#tos'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
