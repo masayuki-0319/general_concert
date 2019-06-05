08499cdafcb03a8acc0e5270e77ead51c83fef3d
将之大西 <omccello@gmail.com>
2019-06-05 20:39:20 +0900

    UsersController生成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/about',   to: 'static_pages#about'
   get '/tos',     to: 'static_pages#tos'
+  get '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
