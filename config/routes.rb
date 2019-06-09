fe48f9bf888abefc57c5fe3e9e0e4d916fb27e23
将之大西 <omccello@gmail.com>
2019-06-09 14:15:13 +0900

    Users#showのView実装，Gravatarの表示実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get '/about',   to: 'static_pages#about'
   get '/tos',     to: 'static_pages#tos'
   get '/signup',  to: 'users#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
