42ed93bb3f32f8e919705cd6edb153fccdd892be
将之大西 <omccello@gmail.com>
2019-06-10 08:28:22 +0900

    SystemSpec生成，capybara設定修正

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,9 +1,18 @@
 RSpec.configure do |config|
-  config.before(:each, type: :system) do
-    driven_by :rack_test
+  config.before(:each) do |example|
+    if example.metadata[:type] == :system
+      if example.metadata[:js]
+        driven_by :selenium_chrome_headless, screen_size: [1400, 1400]
+      else
+        driven_by :rack_test
+      end
+    end
   end
-
-  config.before(:each, type: :system, js: true) do
-    driven_by :selenium_chrome_headless
-  end
+  # 下記はEverydayRails準拠の設定で，上記は伊藤氏のQiitaを参考に下記を編集したもの。
+  # config.before(:each, type: :system) do
+  #   driven_by :rack_test
+  # end
+  # config.before(:each, type: :system, js: true) do
+  #   driven_by :selenium_chrome_headless
+  # end
 end
---
+++ spec/system/users_signup_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.feature 'Users_Signup', type: :system do
+  scenario 'ユーザ登録失敗' do
+  end
+end
