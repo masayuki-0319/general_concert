42ab5a46aa072f8dbec2ae313c93a920777400b3
将之大西 <omccello@gmail.com>
2019-06-11 21:01:38 +0900

    SessionのViewを調整，ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  root  'static_pages#home'
-  get   '/about',   to: 'static_pages#about'
-  get   '/tos',     to: 'static_pages#tos'
-  get   '/signup',  to: 'users#new'
-  post  '/signup',  to: 'users#create'
+  root    'static_pages#home'
+  get     '/about',   to: 'static_pages#about'
+  get     '/tos',     to: 'static_pages#tos'
+  get     '/signup',  to: 'users#new'
+  post    '/signup',  to: 'users#create'
+  get     '/login',   to: 'sessions#new'
+  post    '/login',   to: 'sessions#create'
+  delete  '/logout',  to: 'sessions#destroy'
   resources :users
 end
