953ad6fb23d961baa5397842fdf0022d8b88a077
将之大西 <omccello@gmail.com>
2019-06-11 14:36:14 +0900

    テスト追加：UsersControllerのnewとcreate

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,7 @@
 FactoryBot.define do
   factory :user do
-    name { "main_user" }
-    email { "main_user@example.com" }
-    password { "password" }
-    password_confirmation { "password" }
-  end
-
-  factory :other_user do
-    name { "other_user" }
-    email { "other_user@example.com" }
+    sequence(:name) { |n| "#{n}-user" }
+    sequence(:email) { |n| "#{n}-user@example.com" }
     password { "password" }
     password_confirmation { "password" }
   end
