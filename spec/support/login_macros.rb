91e00688354704ffa08a4967d9f8aa0795ca686b
将之大西 <omccello@gmail.com>
2019-06-18 15:49:32 +0900

    ユーザ編集機能に係るSystemSpec追加

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -5,11 +5,10 @@ module LoginMacros
   end
 
   # テストユーザとしてログイン
-  def log_in_as(user, password: 'password', remember_me: '1')
-    post login_path, params: { session: {
-      email: user.email,
-      password: password,
-      remember_me: remember_me,
-    } }
+  def log_in_as(user)
+    visit login_path
+    fill_in '電子メール', with: user.email
+    fill_in 'パスワード', with: user.password
+    click_button 'ログイン'
   end
 end
--- spec/system/users_edit_spec.rb
+++ spec/system/users_edit_spec.rb
@@ -3,49 +3,38 @@ require 'rails_helper'
 RSpec.feature 'UsersEdit', type: :system do
   let!(:user) { create(:user) }
 
-  background { visit login_path }
-
-  before do
-    fill_in '電子メール', with: user.email
-    fill_in 'パスワード', with: user.password
+  background do
+    log_in_as(user)
+    visit edit_user_path(user)
   end
 
-  context 'ユーザログイン' do
+  before do
+    fill_in '名前', with: 'Name After Changing'
+    fill_in '電子メール', with: 'email@after.com'
+    fill_in 'パスワード', with: user.password
+    fill_in 'パスワード確認', with: user.password
+  end
+
+  context 'ユーザ編集' do
     scenario '通常成功' do
-      click_button 'ログイン'
-      expect(current_path).to eq user_path(User.last)
-      expect(page).to have_content user.name
+      click_button '変更'
+      expect(current_path).to eq user_path(user)
+      expect(page).to have_content 'Name After Changing'
     end
 
-    scenario 'クッキーを保存して成功' do
-      check 'session_remember_me'
-      expect(page).to have_checked_field('このパソコンにログイン情報を保存する。')
-      click_button 'ログイン'
-      expect(current_path).to eq user_path(User.last)
-      expect(page).to have_content user.name
+    scenario 'パスワード欄を空にして成功' do
+      fill_in 'パスワード', with: ''
+      fill_in 'パスワード確認', with: ''
+      click_button '変更'
+      expect(current_path).to eq user_path(user)
+      expect(page).to have_content 'Name After Changing'
     end
 
     scenario '失敗' do
       fill_in '電子メール', with: ''
-      click_button 'ログイン'
-      expect(current_path).to eq login_path
-      expect(page).to have_title 'ログイン'
-      expect(page).to have_css 'div.alert-danger'
-      visit root_path
-      expect(page).not_to have_css 'div.alert-danger'
-    end
-  end
-
-  context 'ユーザログアウト' do
-    scenario '成功' do
-      click_button 'ログイン'
-      click_on 'ログアウト'
-      expect(current_path).to eq root_path
-      expect(page).not_to have_link 'ログアウト'
-      # ２つ目のWindowでログアウト実行時
-      page.driver.submit :delete, '/logout', {}
-      expect(current_path).to eq root_path
-      expect(page).not_to have_link 'ログアウト'
+      click_button '変更'
+      expect(current_path).to eq user_path(user)
+      expect(page).to have_content 'ユーザー情報編集'
     end
   end
 end
