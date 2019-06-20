cdac5d645d3c25324634fb43e21fa675b338e4e6
将之大西 <omccello@gmail.com>
2019-06-20 10:58:45 +0900

    投稿動画のValidation追加

--- app/models/music_post.rb
+++ app/models/music_post.rb
@@ -1,3 +1,8 @@
 class MusicPost < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 140 }
+  VALID_IFRAME_REGEX = /\A<iframe[\w+\-\/:<>=";\. ]+<\/iframe>\z/
+  validates :iframe, presence: true, length: { maximum: 300 },
+                     format: { with: VALID_IFRAME_REGEX }
 end
--- spec/models/music_post_spec.rb
+++ spec/models/music_post_spec.rb
@@ -2,8 +2,59 @@ require 'rails_helper'
 
 RSpec.describe MusicPost, type: :model do
   context '#Validation' do
-    let!(:user) { create(:user) }
+    let!(:music) { create(:music_post) }
 
-    pending
+    it '全属性の有効性' do
+      music.valid?
+      expect(music).to be_valid
+    end
+
+    it 'ユーザの存在性' do
+      music.user_id = nil
+      music.valid?
+      expect(music).not_to be_valid
+    end
+
+    it '投稿動画の存在性（nil時）' do
+      music.iframe = nil
+      music.valid?
+      expect(music).not_to be_valid
+    end
+
+    it '投稿動画の存在性（空白時）' do
+      music.iframe = '  '
+      music.valid?
+      expect(music).not_to be_valid
+    end
+
+    it '投稿動画の文字列の長さ' do
+      music.iframe = 'a' * 301
+      music.valid?
+      expect(music).not_to be_valid
+    end
+
+    it '投稿動画の文字列がYoutubeで取得したiframeか（正規表現）' do
+      music.iframe = "<a>#{'a' * 100}</a>"
+      music.valid?
+      expect(music).not_to be_valid
+    end
+
+    it 'タイトルの存在性（nil時）' do
+      music.title = nil
+      music.valid?
+      expect(music).not_to be_valid
+    end
+
+    it 'タイトルの存在性（空白時）' do
+      music.title = '  '
+      music.valid?
+      expect(music).not_to be_valid
+    end
+
+    it 'タイトルの長さ' do
+      music.title = 'a' * 141
+      music.valid?
+      expect(music).not_to be_valid
+    end
   end
 end
