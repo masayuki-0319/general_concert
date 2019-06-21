4df6ec46b0ffb5bcc2e08dba3004a47e681e3a96
将之大西 <omccello@gmail.com>
2019-06-21 10:33:40 +0900

    動画投稿機能に係るSystemSpec追加

--- spec/controllers/music_posts_controller_spec.rb
+++ spec/controllers/music_posts_controller_spec.rb
@@ -3,14 +3,25 @@ require 'rails_helper'
 RSpec.describe MusicPostsController, type: :controller do
   let!(:user) { create(:user) }
   let!(:music) { create(:music_post, user_id: user.id) }
-  let(:music_attributes) { build(:music_post, user_id: user.id) }
+  let(:music_attributes) { attributes_for(:music_post, user_id: user.id) }
 
   before { session[:user_id] = user.id }
 
   describe 'POST #create' do
-    context 'ログインしていない場合' do
-      subject { post :create, params: { music_post: music_attributes } }
+    subject { post :create, params: { music_post: music_attributes } }
 
+    context 'ログインしている場合' do
+      it '投稿数が増加' do
+        expect { subject }.to change(MusicPost, :count).by(1)
+      end
+
+      it 'TOPページへリダイレクト' do
+        subject
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'ログインしていない場合' do
       before { session[:user_id] = nil }
 
       it '投稿数は増加しない' do
@@ -25,9 +36,20 @@ RSpec.describe MusicPostsController, type: :controller do
   end
 
   describe 'DELETE #destroy' do
-    context 'ログインしていない場合' do
-      subject { delete :destroy, params: { id: music.id } }
+    subject { delete :destroy, params: { id: music.id } }
 
+    context 'ログインしている場合' do
+      it '投稿が削除される' do
+        expect { subject }.to change(MusicPost, :count).by(-1)
+      end
+
+      it 'トップページへリダイレクト' do
+        subject
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'ログインしていない場合' do
       before { session[:user_id] = nil }
 
       it '投稿が削除されない' do
---
+++ spec/system/music_posts_interface_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.feature 'MusicPostInterface', type: :system do
+  let(:user) { create(:user) }
+  let(:other_user) { create(:user) }
+  let!(:music) { create(:music_post, user_id: user.id) }
+  let!(:other_music) { create(:music_post, user_id: other_user.id) }
+
+  background do
+    log_in_as(user)
+  end
+
+  scenario 'ユーザが動画を投稿する。' do
+    visit root_path
+    fill_in 'music_post_title', with: 'TestTitle'
+    fill_in 'music_post_iframe', with: '<iframe width="560" height="315" src="https://www.youtube.com/embed/daS06STpEx4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    click_on '投稿'
+    expect(page).to have_content 'TestTitle'
+    expect(page).to have_css 'iframe', count: 2
+    expect(page).to have_content "#{user.music_posts.count}件"
+  end
+
+  scenario 'ユーザが動画を削除する。' do
+    visit root_path
+    within("#music_post-#{music.id}") do
+      expect(page).to have_content music.title
+      click_on '動画を削除'
+    end
+    expect(page).not_to have_content music.title
+    expect(page).not_to have_css 'iframe'
+    expect(page).to have_content "#{user.music_posts.count}件"
+  end
+
+  scenario '他ユーザの投稿動画は削除できない。' do
+    visit user_path(other_user)
+    expect(page).not_to have_button '動画を削除'
+  end
+end
