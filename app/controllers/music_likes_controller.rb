b07050b778e0d1833c3b6591060a122e2e464037
将之大西 <omccello@gmail.com>
2019-07-06 09:27:30 +0900

    いいね！機能Ajax化

--- app/controllers/music_likes_controller.rb
+++ app/controllers/music_likes_controller.rb
@@ -2,12 +2,20 @@ class MusicLikesController < ApplicationController
   before_action :logged_in_user
 
   def create
-    current_user.like(MusicPost.find(params[:music_post_id]))
-    redirect_to request.referrer || root_path
+    @music_post_id = MusicPost.find(params[:music_post_id])
+    current_user.like(@music_post_id)
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_path }
+      format.js
+    end
   end
 
   def destroy
+    @music_post_id = MusicPost.find(params[:music_post_id])
     current_user.unlike(MusicPost.find(params[:music_post_id]))
-    redirect_to request.referrer || root_path
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_path }
+      format.js
+    end
   end
 end
