9b5fe4a28b8e633ddf4ade65230ec62d7e5f7189
将之大西 <omccello@gmail.com>
2019-07-09 20:53:29 +0900

    fakerの名前変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ User.create!(name:  "Example User",
              admin: true)
 
 99.times do |n|
-  name  = Faker::Pokemon.name
+  name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
