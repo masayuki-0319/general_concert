629511fc2f760d1761b31426fa4028a61a44177d
将之大西 <omccello@gmail.com>
2019-07-11 08:14:55 +0900

    MusicComment:Ajax実装

--- app/controllers/music_comments_controller.rb
+++ app/controllers/music_comments_controller.rb
@@ -3,15 +3,22 @@ class MusicCommentsController < ApplicationController
 
   def create
     @music_post = MusicPost.find(params[:id])
-    current_user.music_comments.create(music_post_id: @music_post.id,
-                                       comment: params[:music_comment][:comment])
-    redirect_to @music_post
+    @music_comment = current_user.
+      music_comments.create(music_post_id: @music_post.id,
+                            comment: params[:music_comment][:comment])
+    respond_to do |format|
+      format.html { redirect_to @music_post }
+      format.js
+    end
   end
 
   def destroy
     @music_comment = MusicComment.find(params[:id])
     @music_post = MusicPost.find(@music_comment.music_post_id)
     @music_comment.destroy
-    redirect_to @music_post
+    respond_to do |format|
+      format.html { redirect_to @music_post }
+      format.js
+    end
   end
 end
